b0967523e2a5b0f46ac09f369de24074a5688fad
nameofname <nameofemail@gmail.com>
2017-06-13 19:48:40 -0400

    started on script to clone all repos

--- src/cloneAllRepos.js
+++ src/cloneAllRepos.js
@@ -2,5 +2,14 @@
 
 const nodegit = require('nodegit');
 const allRepos = require('../allRepos.json');
+const allGitUrls = allRepos.map(o => o.git_url);
+const path = require('path');
 
-nodegit.
+// TODO ! This is for immediate safety :
+allGitUrls.splice(3, allGitUrls.length);
+// TODO ! This is for immediate safety :
+
+
+allGitUrls.forEach(url => {
+    nodegit.clone(url, path.resolve(`${__dirname}/../`))
+});
