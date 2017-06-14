6ed4907a6d197eed9500c5fd2e03d7ad6608066c
nameofname <nameofemail@gmail.com>
2017-06-13 20:29:08 -0400

    working on script to clone repos

--- src/cloneAllRepos.js
+++ src/cloneAllRepos.js
@@ -1,15 +1,62 @@
 "use strict";
 
-const nodegit = require('nodegit');
-const allRepos = require('../allRepos.json');
-const allGitUrls = allRepos.map(o => o.git_url);
+require('dotenv')();
 const path = require('path');
+const fs = require('fs');
+const Git = require('nodegit');
+let allRepos;
+try {
+    allRepos = require('../allRepos.json');
+} catch (e) {
+    console.log('PLEAS READ : allRepos.json file doesn\'t exist. Run "yarn updateRepoList"\n', e);
+    throw e;
+}
+allRepos = allRepos.filter(o => !!o.clone_url); // filter out urls that are empty or undefined
+const cloneOptions = {
+    fetchOpts: {
+        callbacks: {
+            certificateCheck: () => 1,
+            credentials: () => NodeGit.Cred.userpassPlaintextNew(process.env.GIT_USER, GIT_PW)
+        }
+    }
+};
+
+// const cloneOptions = {
+//     remoteCallbacks: {
+//         credentials: (url, userName) => {
+//             return Git.Cred.sshKeyFromAgent(userName);
+//         }
+//     }
+// };
+
 
 // TODO ! This is for immediate safety :
-allGitUrls.splice(3, allGitUrls.length);
+allRepos.splice(3, allRepos.length);
 // TODO ! This is for immediate safety :
 
 
-allGitUrls.forEach(url => {
-    nodegit.clone(url, path.resolve(`${__dirname}/../`))
-});
+const cloneThem = () => {
+    const promises = [];
+
+    allRepos.forEach(o => {
+        const { clone_url, name } = o;
+        const dir = path.resolve(`${__dirname}/../repos/${name}`);
+        if (!fs.existsSync(dir)) {
+            console.log(`cloning ${name} into ${dir} via ${clone_url}`);
+            promises.push(Git.Clone(clone_url, dir, cloneOptions));
+        } else {
+            console.log(`skipping ${dir} - it already exists`);
+        }
+    });
+
+    Promise.all(promises)
+        .then(() => {
+            console.log('All repos cloned');
+        })
+        .catch(e => {
+            console.log("Error cloning repo");
+            throw e;
+        })
+};
+
+module.exports = cloneThem;
