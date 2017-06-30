15239e1b7324c2a26a3df0bf318db8c8fd5cb131
nameofname <nameofemail@gmail.com>
2017-06-30 13:23:14 -0400

    added support for circular dependency detection

--- src/buildDependentTree.js
+++ src/buildDependentTree.js
@@ -4,11 +4,19 @@ const shallowTree = require('./buildShallowTree')();
 const logger = require('./logger');
 
 
-const buildTreeRecur = (currNode) => {
+const buildTreeRecur = (currNode, pathToNode) => {
+
+    const occurrences = pathToNode.filter(n => n === currNode.packageName).length;
+    if (occurrences > 1) {
+        log.warn(`found circular dependency for ${currNode.packageName}, path : ${JSON.stringify(pathToNode)}`);
+        return;
+    }
 
     Object.keys(currNode.dependents).forEach(dependentName => {
         currNode.dependents[dependentName].dependents = shallowTree[dependentName].dependents;
-        return buildTreeRecur(currNode.dependents[dependentName]);
+        currNode.dependents[dependentName].packageName = dependentName;
+        const newPathToNode = [...pathToNode, dependentName];
+        return buildTreeRecur(currNode.dependents[dependentName], newPathToNode);
     });
 
     return currNode;
@@ -23,5 +31,5 @@ module.exports = packageName => {
     }
     logger.trace(packageShallow);
 
-    return buildTreeRecur(packageShallow);
+    return buildTreeRecur(packageShallow, [packageName]);
 };
